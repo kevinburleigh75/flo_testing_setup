241573ba4443d4cc5483249f4039ecd084485b00
Kevin Burleigh <klb@kindling>
2013-09-23 15:54:15 -0700

    clean rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AndroidAppServer::Application.config.secret_token = '9d354ec47bcc5a96b2106371c4b55e51554be4435116aa2a17da673c258c3f2922cca98d6dd0643dcb8b35bbea8496cbe4019658cb94ce8a1f010174cc64a67b'
